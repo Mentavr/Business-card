1995e917a130f66ea36e4b19a774734831395184
Artem Bykov <bykovaa94@mail.ru>
2025-08-26 15:06:10 +0300

    fix: fix copirate

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -9,27 +9,27 @@ const Footer = () => {
     return (
         <footer
             id="contacts"
-            className="container pt-[64px] pb-[64px] tablet:pt-[122px] tablet:pb-[122px]"
+            className="container pt-[4rem] pb-[1.25rem] tablet:pb-[4rem] tablet:pt-[7.625rem] tablet:pb-[7.625rem]"
         >
             <div className="flex flex-col justify-around gap-16 tablet:flex-row tablet:gap-10 desktop:gap-20 ">
                 <div className="flex flex-col tablet:w-[50%]">
                     <h2 className="text-[3.5625rem]  leading-[90%]  desktop:text-[5.625rem] mb-[16px]">
                         {t('footer.title')}
                     </h2>
-                    <div className="flex gap-[5px] text-[18px] text-gray flex-wrap mb-[20px]">
+                    <div className="flex gap-[0.3125rem] text-[1.125rem] text-gray flex-wrap mb-[1.25rem]">
                         <span> {t('footer.description')}</span>
                         <a
                             href="mailto:mentarv94@gmail.com"
                             target="_blank"
-                            className="cursor-pointer block w-max border-b-[2px] text-yellow border-yellow hover:text-[orange] hover:border-[orange]"
+                            className="cursor-pointer block w-max border-b-[0.125rem] text-yellow border-yellow hover:text-[orange] hover:border-[orange]"
                         >
                             mentarv94@gmail.com
                         </a>
                     </div>
-                    <div className="flex gap-[5px]  text-[18px] text-gray mb-[40px] flex-wrap">
+                    <div className="flex gap-[0.3125rem]  text-[1.125rem] text-gray mb-[2.5rem] flex-wrap">
                         <span>{t('footer.resumeTitle')}</span>
                         <a
-                            className="cursor-pointer block w-max border-b-[2px] text-yellow border-yellow hover:text-[orange] hover:border-[orange]"
+                            className="cursor-pointer block w-max border-b-[0.125rem] text-yellow border-yellow hover:text-[orange] hover:border-[orange]"
                             href="/resume.pdf"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -50,7 +50,7 @@ const Footer = () => {
                     <SendEmail />
                 </div>
             </div>
-            <span className="block mt-[20px]">{`© ${new Date(Date.now()).getFullYear()} ${t('footer.name')}`}</span>
+            <span className="block mt-[2.5rem] tablet:mt-[1.25rem]">{`© ${new Date(Date.now()).getFullYear()} ${t('footer.name')}`}</span>
         </footer>
     );
 };
